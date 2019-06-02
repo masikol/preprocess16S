76d6f230ccd0e9abcfe0ae649b11efa12f0dace6
Deynonih <maximdeynonih@gmail.com>
2019-06-02 21:23:43 +0300

    beautiful colorful installator

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set -e # abort installation if something goes pear-shaped
+# people want to see beautiful colorful installator
+YELLOW="\e[1;33m"
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+ENDCOLOR="\e[0m"
 
-echo -e 'Installation started\n'
+echo -e "\n${GREEN}Installation started\n${ENDCOLOR}"
 
 startdir=`pwd`
 
@@ -14,13 +18,13 @@ const_V3_V4_nameonly=constant_region_V3-V4.fasta
 const_V3_V4_abspath=`realpath $const_V3_V4_nameonly`
 
 for file in $read_merging_module $const_V3_V4_nameonly $setup_py_path; do
-    echo "Checking existance of $file..."
+    echo "Checking existance of $file"
     if [[ ! -f $file ]]; then
-        echo "Cannot find $file"
-        echo "Please, make sure that $file is located in current directory"
+        echo -e "${RED}Cannot find $file \a"
+        echo -e "Please, make sure that $file is located in current directory${ENDCOLOR}"
         exit 1
     fi
-    echo -e "ok...\n"
+    echo -e "${GREEN}ok...\n${ENDCOLOR}"
 done
 
 
@@ -29,30 +33,33 @@ done
 for utility in fasta36 blastn blastdbcmd; do
     echo "Checking $utility..."
     if [[ -z `which $utility` ]]; then
-        echo "Attention! $utility is required to use read merging tool."
-        echo "Please, make sure that $utility is installed on your computer if you want to use it." 
+        echo -e "${YELLOW}Attention! $utility is required to use read merging tool."
+        echo -e "Please, make sure that $utility is installed on your computer if you want to use it.${ENDCOLOR}" 
     else
-        echo -e "ok...\n"
+        echo -e "${GREEN}ok...\n${ENDCOLOR}"
     fi
 done
 
 echo "Checking gnuplot..."
 if [[ -z `which gnuplot` ]]; then
-    echo "Attention! gnuplot is required to use plotting tool."
-    echo "Please, make sure that gnuplot is installed on your computer if you want to use it." 
+    echo "{YELLOW}Attention! gnuplot is required to use plotting tool."
+    echo "Please, make sure that gnuplot is installed on your computer if you want to use it.${ENDCOLOR}" 
 else
-    echo -e "ok...\n"
+    echo -e "${GREEN}ok...\n${ENDCOLOR}"
 fi
 
 echo "Checking makeblastdb..."
 if [[ -z `which makeblastdb` ]]; then
-    echo "Attention! makeblastdb is required to use this tools."
+    echo -e "${RED}Attention! makeblastdb is required to use this tools.\a"
     echo "Please, make sure that makeblastdb is installed on your computer and after that try installation again"
-    echo 'If this error still occure although you have installed everything -- make sure that all these programs are added to PATH' 
+    echo "If this error still occure although you have installed everything -- make sure that all these programs are added to PATH${ENDCOLOR}"
     exit 1
 else
-    echo -e  "ok...\n"
+    echo -e "${GREEN}ok...\n${ENDCOLOR}"
 fi
+
+# after above checks we need to abort installation if somethins goes pear-shaped:
+set -e
 
 make_db=`which makeblastdb`
 
@@ -63,7 +70,8 @@ if [[ $1 = '-o' && -n $2 ]]; then
 else
     db_dir=SILVA_DB
 fi
-echo -e "\nSilva database will be placed in the following directory: `realpath $db_dir`\n"
+echo -e "\n${YELLOW}Silva${ENDCOLOR} database will be placed in the following directory:"
+echo -e "\t${YELLOW} `realpath $db_dir` ${ENDCOLOR}\n"
 mkdir $db_dir
 
 
@@ -75,20 +83,23 @@ db_link=https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA
 db_gzipped=SILVA_132_SSURef_Nr99_tax_silva.fasta.gz
 db_name=SILVA_132_SSURef_Nr99_tax_silva.fasta
 
-echo 'Database downloading started'; echo ''
+echo -e "Database downloading started\n"
 wget $db_link
+echo -e "${GREEN}Database is successfully downloaded${ENDCOLOR}\n"
 
 
 # === Gunzip archive === 
 
 echo -e 'Unpacking database file...\n'
 gunzip $db_gzipped
-echo 'Unpacking is completed.'
+echo -e "${GREEN}Unpacking is completed.${ENDCOLOR}"
 
 
 # === Make a database ===
 
 $make_db -in $db_name -parse_seqids -dbtype nucl
+
+echo -e "\n${GREEN} Silva database is successfully configured ${ENDCOLOR}"
 
 db_abspath=`realpath $db_name`
 
@@ -100,7 +111,7 @@ constV3V4_abspath=`realpath $const_V3_V4_nameonly`
 
 # === Configure module 'read_merging_16S' by specifying locations of files needed for it's work ===
 
-echo -en "\nConfiguring module $read_merging_module...";
+echo -en "\nConfiguring 'read_merging_16S' module...";
 
 sed "s|REPLACE_DB|$db_abspath|g" $read_merging_module > buff.txt
 cat buff.txt > $read_merging_module
@@ -108,7 +119,7 @@ cat buff.txt > $read_merging_module
 sed "s|REPLACE_CONST|$constV3V4_abspath|g" $read_merging_module > buff.txt
 cat buff.txt > $read_merging_module
 
-echo -e 'done.\n'
+echo -e "${GREEN}done.\n${ENDCOLOR}"
 
 rm buff.txt
 
@@ -121,11 +132,11 @@ cd $startdir
 
 python3 $setup_py_path install
 
-echo -e "Done\n"
+echo -e "\n${GREEN}'read_merging_16S' module is successfully installed\n${ENDCOLOR}"
 
 rm __init__.py
 rm $setup_py_path
 rm -r build/
 
-echo 'Installation succeeded'
+echo -e "${GREEN}Installation succeeded${ENDCOLOR}\n\n"
 rm install.sh
